7f2c20ef1d5926d49c4d65c43f3d5ca28ebd4677
williscs <you@example.com>
2020-08-14 12:43:28 +0100

    Add all files

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
+
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({keys: ["wrpgrgfccsI9jIH77hohgg"]}));
 
 app.use(authRouter);
+app.use(productsRouter);
 
 
 app.listen(3000, () => {
